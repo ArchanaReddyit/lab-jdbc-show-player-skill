6157da2ad12880857cfd600d5b981d4dfebde5dd
ArchanaReddyit <archanareddy.it@gmail.com>
2020-08-20 07:42:35 +0400

    Done

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,42 @@
 
+package controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+//			//int x;
+//			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//			//x=Integer.parseInt(br.readLine());
+//			
+//			//objects  which progression
+//			
+//			SkillDAO skilldao=new SkillDAO();
+//
+//			System.out.println("Enter your ID");
+//			Long id=Long.parseLong(br.readLine());
+//			System.out.println("Enter Names");
+//			String name=br.readLine();
+//				
+//				Skill.getSkillid();
+//				Skill.getSkillName();
+//
+//				Skill Skill = new Skill(id, name); 
+		SkillDAO skilldao=new SkillDAO();
+				List<Skill> list = skilldao.listAllSkillDAO();
+				
+				System.out.println(list);
+				
+			
+			}
+	}
+
+
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,65 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	
+	public List<Skill> listAllSkillDAO () throws Exception {
+		
+		System.out.println("Create Skill dao");
+	List<Skill> list = new ArrayList<Skill>();
+//		long ID = Skill.getSkillid();
+//		String NAMES=Skill.getSkillName();
+		
+//		ConnectionManager cm=new ConnectionManager();
+//		
+//        
+//	String sql="Insert into Skill(ID,NAMES) VALUES(?,?)";
+//	
+//		PreparedStatement st=ConnectionManager.getConnection().prepareStatement(sql);
+//		st.setLong(1, ID);
+//		st.setString(2, NAMES);
+//		
+//		System.out.println("established");
+//		
+//		int x =st.executeUpdate();
+//		System.out.println(x); we hav to use select and return those right yes aodd in array list and resturn ok i will do or i will do u do ok
+		
+		
+		String Skills="select ID, NAMES from skills order by NAMES ASC";
+		
+	 Connection con=null;
+		   try {
+			con=ConnectionManager.getConnection();
+//			 PreparedStatement prep=con.prepareStatement(Skills);
+//			  prep.setLong(1, ID);
+//			 prep.setString(2, Skill.getSkillName());
+//			  ResultSet rs=prep.executeQuery();
+			
+			Statement statement = con.createStatement();
+			ResultSet resultSet = statement.executeQuery(Skills);
+		while(resultSet.next()) {
+			Long id = resultSet.getLong("ID");
+			String names = resultSet.getString("NAMES");
+			Skill skill = new Skill(id, names);
+			list.add(skill);
+		}
+		   } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  
+		   return list;
+		   
+	   }
+
+	
+}
